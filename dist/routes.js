f4477b2a9012332508684d5c55c20cf36ef00bcb
Laith Al-Helwane <laith.helwany2@gmail.com>
2023-01-03 12:29:53 +0300

    fix

--- dist/routes.js
+++ dist/routes.js
@@ -3,15 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const client_router_1 = __importDefault(require("./router/client.router"));
 const owner_router_1 = __importDefault(require("./router/owner.router"));
 const delivery_router_1 = __importDefault(require("./router/delivery.router"));
 const admin_router_1 = __importDefault(require("./router/admin.router"));
+const swagger_json_1 = __importDefault(require("../swagger.json"));
 function routes(app) {
     app.use('/c', client_router_1.default);
     app.use('/o', owner_router_1.default);
     app.use('/d', delivery_router_1.default);
     app.use('/a', admin_router_1.default);
+    app.use('/api-doc', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 }
 exports.default = routes;
 //# sourceMappingURL=routes.js.map
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Express } from 'express';
+import swaggerUi from 'swagger-ui-express'
 import clientRouter from './router/client.router';
 import ownerRouter from './router/owner.router';
 import deliveryRouter from './router/delivery.router';
 import adminRouter from './router/admin.router';
+import swaggerDoc from '../swagger.json';
 export default function routes(app: Express) {
   app.use('/c', clientRouter);
   app.use('/o', ownerRouter);
   app.use('/d', deliveryRouter);
   app.use('/a', adminRouter);
+  app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc) )
 }
